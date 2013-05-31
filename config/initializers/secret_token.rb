f37420943dd205048cbc8e198477aae66af4418f
Stefan Keil <an.stefan.keil@googlemail.com>
2013-05-31 14:34:50 +0200

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlowerBoxV1::Application.config.secret_key_base = '8ddcf3b72ccc7526718e2a11ee1663fd71da26283e0764c52398fb2c9770bda3f2de0bed680a6a5c523291463c468d669c1dca4b7a6018bbf05045fc89be2cd7'
